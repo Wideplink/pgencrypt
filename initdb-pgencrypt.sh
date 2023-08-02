81bdbf4a7b58debcbadc0342447ea719fde8b6fd
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-08-02 22:32:41 +0900

    chore: add editorconfig

--- initdb-pgencrypt.sh
+++ initdb-pgencrypt.sh
@@ -7,8 +7,8 @@ EOSQL
 
 # Load pgencrypt into both template_database and $POSTGRES_DB
 for DB in template_pgencrypt "$POSTGRES_DB"; do
-        echo "Loading pgencrypt extensions into $DB"
-        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-                CREATE EXTENSION IF NOT EXISTS pgencrypt;
-		EOSQL
+    echo "Loading pgencrypt extensions into $DB"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+        CREATE EXTENSION IF NOT EXISTS pgencrypt;
+EOSQL
 done
